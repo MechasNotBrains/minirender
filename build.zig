0a6b99ba5f63fc5a3803d694b3eee3a338ec77ef
sOkam <7308253+heysokam@users.noreply.github.com>
2026-07-16 08:10:00 +0200

    bld: zig: Add mmath dependency

--- build.zig
+++ build.zig
@@ -5,11 +5,14 @@ pub fn build(b: *@import("std").Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const mmath_dep = b.dependency("mmath", .{ .target = target, .optimize = optimize });
+
     const mod = b.addModule("minirender", .{
         .root_source_file = b.path("src/minirender.zig"),
         .target = target,
         .optimize = optimize,
     });
+    mod.addImport("mmath", mmath_dep.module("mmath"));
     mod.linkSystemLibrary("glfw", .{});
     mod.linkSystemLibrary("GL", .{});
 }
