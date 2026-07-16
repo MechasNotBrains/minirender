af9d090597aa6bf8ab136a95c651e9e93d38539a
sOkam <7308253+heysokam@users.noreply.github.com>
2026-07-16 12:19:53 +0200

    bld: zig: Update builder setup and dependencies

--- build.zig
+++ build.zig
@@ -1,18 +1,25 @@
 //:_______________________________________________________________________
 //  minirender  |  Copyright (C) Ivan Mar (sOkam!)  |  GPL-3.0-or-later  :
 //:_______________________________________________________________________
-pub fn build(b: *@import("std").Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+pub fn build(B :*@import("std").Build) void {
+  const target   = B.standardTargetOptions(.{});
+  const optimize = B.standardOptimizeOption(.{});
 
-    const mmath_dep = b.dependency("mmath", .{ .target = target, .optimize = optimize });
+  const mmath_dep = B.dependency("mmath", .{.target= target, .optimize= optimize });
+  const mcam_dep  = B.dependency("mcam",  .{.target= target, .optimize= optimize });
+  const msys_dep  = B.dependency("msys",  .{.target= target, .optimize= optimize });
+  const mgl_dep   = B.dependency("mgl",   .{.target= target, .optimize= optimize });
+  const minp_dep  = B.dependency("minp",  .{.target= target, .optimize= optimize });
 
-    const mod = b.addModule("minirender", .{
-        .root_source_file = b.path("src/minirender.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    mod.addImport("mmath", mmath_dep.module("mmath"));
-    mod.linkSystemLibrary("glfw", .{});
-    mod.linkSystemLibrary("GL", .{});
+  const mod = B.addModule("minirender", .{
+    .root_source_file = B.path("src/minirender.zig"),
+    .target           = target,
+    .optimize         = optimize,
+  });
+  mod.addImport("mmath", mmath_dep.module("mmath"));
+  mod.addImport("mcam",  mcam_dep.module("mcam"));
+  mod.addImport("msys",  msys_dep.module("msys"));
+  mod.addImport("mgl",   mgl_dep.module("mgl"));
+  mod.addImport("minp",  minp_dep.module("minp"));
 }
+
