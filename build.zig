f2333b9a1de627ee26dd2376b7c2c4ea7766d422
sOkam <7308253+heysokam@users.noreply.github.com>
2026-07-16 13:15:55 +0200

    bld: zig: Add mui as a dependency

--- build.zig
+++ build.zig
@@ -5,21 +5,23 @@ pub fn build(B :*@import("std").Build) void {
   const target   = B.standardTargetOptions(.{});
   const optimize = B.standardOptimizeOption(.{});
 
-  const mmath_dep = B.dependency("mmath", .{.target= target, .optimize= optimize });
-  const mcam_dep  = B.dependency("mcam",  .{.target= target, .optimize= optimize });
-  const msys_dep  = B.dependency("msys",  .{.target= target, .optimize= optimize });
-  const mgl_dep   = B.dependency("mgl",   .{.target= target, .optimize= optimize });
-  const minp_dep  = B.dependency("minp",  .{.target= target, .optimize= optimize });
+  const mmath = B.dependency("mmath", .{.target= target, .optimize= optimize });
+  const mcam  = B.dependency("mcam",  .{.target= target, .optimize= optimize });
+  const msys  = B.dependency("msys",  .{.target= target, .optimize= optimize });
+  const mgl   = B.dependency("mgl",   .{.target= target, .optimize= optimize });
+  const minp  = B.dependency("minp",  .{.target= target, .optimize= optimize });
+  const mui   = B.dependency("mui",   .{.target= target, .optimize= optimize });
 
   const mod = B.addModule("minirender", .{
     .root_source_file = B.path("src/minirender.zig"),
     .target           = target,
     .optimize         = optimize,
   });
-  mod.addImport("mmath", mmath_dep.module("mmath"));
-  mod.addImport("mcam",  mcam_dep.module("mcam"));
-  mod.addImport("msys",  msys_dep.module("msys"));
-  mod.addImport("mgl",   mgl_dep.module("mgl"));
-  mod.addImport("minp",  minp_dep.module("minp"));
+  mod.addImport("mmath", mmath.module("mmath"));
+  mod.addImport("mcam",  mcam.module("mcam"));
+  mod.addImport("msys",  msys.module("msys"));
+  mod.addImport("mgl",   mgl.module("mgl"));
+  mod.addImport("minp",  minp.module("minp"));
+  mod.addImport("mui",   mui.module("mui"));
 }
 
